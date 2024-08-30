6adb4014399201b646cd36749858acfed8ee2bf3
Parveen Kaur <parveenkaur1045@gmail.com>
2024-08-29 18:45:28 -0600

    istalled router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import SignUpPage from "./pages/SignUpPage";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import "./lightTheme.css";
+import { useState } from "react";
+
+//import "bootstrap/dist/css/bootstrap.min.css";
+
 const App = () => {
+  const [theme, setTheme] = useState("");
+
   return (
-    <>
+    <div data-theme={theme}>
+      <button
+        onClick={() =>
+          setTheme((currentTheme) => (currentTheme === "dark" ? " " : "dark"))
+        }
+      >
+        Toggle Mode
+      </button>
+      <br></br>
+
       <SignUpPage />
-    </>
+    </div>
   );
 };
 
